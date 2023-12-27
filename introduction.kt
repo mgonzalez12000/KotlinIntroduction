78e8a8164231e442d9b8e12cfa0deaaa47651b0e
Marco Gonzalez <43226912+mgonzalez12000@users.noreply.github.com>
2023-12-27 02:16:28 -0800

    Kotlin Intro

--- introduction.kt
+++ introduction.kt
@@ -1,3 +1,34 @@
+// In order to excecute the file, code should be ran on the main function
 fun main() {
+    // This is how you write comments in Kottlin
+    /*
+    This is how you can write mult-line comments in Kotlin
+    The format of writing comments in Kotlin is very similar to Java
+    The syntax for Kotlin is very similar to Java and JS
+     */
     println("Hello World")
+    
+    /*
+    VARIABLES
+    --------
+    - Unlike other strictly written languages, when declaring variables in Kotlin you do NOT need to declare the type for the variable.
+    - When declaring variables, Kotlin uses two ways to declare variables: var AND val:
+            Use val for a variable that is immutable (never changes)
+            Use var for a variable that is mutable (value can change)
+      This concept is SIMILAR to JS variable declaration.
+     */
+     var name = "John"
+     val birthYear = 1975
+     println(name)
+     println(birthYear)
+     // If you insist to declare the variable type, you can do the following syntax:
+     var name2: String = "John2"
+     val birthYear2: Int = 1975
+     println(name2)
+     println(birthYear2)
+
+     /*
+     FOR LOOP in Kotlin
+      */
+      
 }
